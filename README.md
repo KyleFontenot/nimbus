125c3d9274bd46d947d45378118680e0e679851b
KyleFontenot <kylewilliamfontenot@gmail.com>
2024-11-29 15:35:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nimbus 
+A Svelte 5 component library that does not require Tailwind. All components I have made myself from scratch cumulatively from all of my projects. 
+
---
+++ utils/httpStatusCodes.ts
@@ -0,0 +1,28 @@
+enum HTTPStatusCodes {
+  OK = 200,
+  Created = 201,
+  Accepted = 202,
+  NonAuthoritativeInformation = 203,
+  NoContent = 204,
+  ResetContent = 205,
+  PartialContent = 206,
+  MultiStatus = 207,
+  AlreadyReported = 208,
+  IMUsed = 226,
+  MultipleChoices = 300,
+  MovedPermanently = 301,
+  Found = 302,
+  SeeOther = 303,
+  NotModified = 304,
+  UseProxy = 305,
+  TemporaryRedirect = 307,
+  PermanentRedirect = 308,
+  BadRequest = 400,
+  Unauthorized = 401,
+  PaymentRequired = 402,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  NotAcceptable = 406
+}
+export default HTTPStatusCodes
